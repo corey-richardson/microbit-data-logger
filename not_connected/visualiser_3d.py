f6ffd31a696cbcfe22265f1ed17a9a966392c17c
Corey Richardson <richardsoncorey04@gmail.com>
2023-06-07 20:06:45 +0100

    Update visualiser_3d.py

--- not_connected/visualiser_3d.py
+++ not_connected/visualiser_3d.py
@@ -5,13 +5,25 @@ import matplotlib.pyplot as plt
 file = fd.askopenfilename(
     filetypes=[("CSV files", "*.csv")],title="Set input .csv file" )
 
-data = pd.read_csv(file, delimiter=".")
+data = pd.read_csv(file, header=0, delimiter=",")
 
 x, y, z = data.x, data.y, data.z
 
 fig = plt.figure()
 
 ax = plt.axes(projection='3d')
-ax.plot3D(x, y, z, color='k')
+ax.plot3D(x, y, z)
+
+ax.set_xlim(-1500, 1500)
+ax.set_ylim(-1500, 1500)
+ax.set_zlim(-1500, 1500)
+
+# # Rotating
+# angle = 0
+# while True:
+#    ax.view_init(30, angle%360)
+#    angle+=1
+#    plt.draw()
+#    plt.pause(.001)
 
 plt.show()
