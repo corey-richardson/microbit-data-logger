100e5fa0f18f20ee014a428094a3ac1395208af2
Corey Richardson <richardsoncorey04@gmail.com>
2023-06-07 19:56:57 +0100

    Update visualiser.py

--- not_connected/visualiser.py
+++ not_connected/visualiser.py
@@ -6,31 +6,22 @@ from scipy import signal
 file = fd.askopenfilename(
     filetypes=[("CSV files", "*.csv")],title="Set input .csv file" )
 
-data = pd.read_csv(file, delimiter=".")
+data = pd.read_csv(file, header=0, delimiter=",").reset_index()
 
-# Get maximum magnitude + 10% for use as y limit
-max_x, max_y, max_z = max(data.x), max(data.y), max(data.z)
-min_x, min_y, min_z = min(data.x), min(data.y), min(data.z)
-min_x, min_y, min_z = abs(min_x), abs(min_y), abs(min_z)
-max_strength, min_strength = max(data.strength), min(data.strength)
-limit = max(max_x, max_y, max_z, min_x, min_y, min_z, max_strength, min_strength)
-limit *= 1.1
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
 
-plt.plot(data["Time (seconds)"], data.x, "r", label="X")
-plt.plot(data["Time (seconds)"], data.y, "g", label="Y")
-plt.plot(data["Time (seconds)"], data.z, "b", label="Z")
-
-plt.plot(data["Time (seconds)"], data.strength, "k", label="strength")
+ax.plot(data.index, data.x, "r", label="X")
+ax.plot(data.index, data.y, "g", label="Y")
+ax.plot(data.index, data.z, "b", label="Z")
 
 plt.title("micro:bit Data Logger")
 plt.ylabel("Magnitude")
-plt.ylim(-limit, limit)
+plt.ylim(-1500, 1500)
 plt.legend()
 
-peaks = signal.find_peaks(data.strength)
-print(peaks)
-
-# for peak in peaks:
-#   plt.axvline(x = peak, color = 'pink')
+peaks, props = signal.find_peaks(data.strength, threshold=150)
+for peak in peaks:
+    plt.axvline(peak, color="k")
 
 plt.show()
