48339892707638f4d7ff95a97bac7710c5c2f0c8
Corey Richardson <richardsoncorey04@gmail.com>
2023-06-12 13:10:25 +0000

    Changed x-axis from index to Time

--- via_csv_data/visualiser.py
+++ via_csv_data/visualiser.py
@@ -8,34 +8,34 @@ from matplotlib import pyplot as plt
 
 # Open file explorer window for user to select CSV Data
 file = fd.askopenfilename(
-    filetypes=[("CSV files", "*.csv")],title="Set input .csv file" )
+    filetypes=[("CSV files", "*.csv")], title="Set input .csv file" )
 # Read the selected data into a dataframe
 # .reset_index() creates an index column
-data = pd.read_csv(file, header=0).reset_index()
-
+data = pd.read_csv(file, header=0)
+data.time = data["Time (seconds)"] - min(data["Time (seconds)"])
 # Create the figure to plot to
 fig = plt.figure(figsize=(10, 10))
 ax = fig.add_subplot(2, 1, 1)
 d_ax = fig.add_subplot(2, 1, 2)
 
 # Plot the data
-ax.plot(data.index, data.x, "r", label="X")
-ax.plot(data.index, data.y, "g", label="Y")
-ax.plot(data.index, data.z, "b", label="Z")
+ax.plot(data.time, data.x, "r", label="X")
+ax.plot(data.time, data.y, "g", label="Y")
+ax.plot(data.time, data.z, "b", label="Z")
 
 data['dx'] = data['x'] - data['x'].shift(-1)
 data['dy'] = data['y'] - data['y'].shift(-1)
 data['dz'] = data['z'] - data['z'].shift(-1)
 
-d_ax.plot(data.index, data.dx, "r", label="X", alpha=0.5)
-d_ax.plot(data.index, data.dy, "g", label="Y", alpha=0.5)
-d_ax.plot(data.index, data.dz, "b", label="Z", alpha=0.5)
+d_ax.plot(data.time, data.dx, "r", label="X", alpha=0.5)
+d_ax.plot(data.time, data.dy, "g", label="Y", alpha=0.5)
+d_ax.plot(data.time, data.dz, "b", label="Z", alpha=0.5)
 
 # Set title, ylabel, y-axis bound limits
 # Define a legend
 ax.set_title("micro:bit Data Logger")
 d_ax.set_title("Deltas")
-plt.ylabel("Magnitude")
+plt.xlabel("Time (s)")
 plt.ylim(-1500, 1500)
 plt.legend()
 
